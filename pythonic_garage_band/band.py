33b3d0f7b3e88f7260706c9f358dd1289547a966
Alec Torres <atorresla24@gmail.com>
2022-07-21 20:27:56 -0400

    lab done

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,10 +1,72 @@
 class Band:
+    members = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        self.instances.append(self)
+
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return solos
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
 
 class Musician:
+    def __init__(self, name):
+        self.name = name
 
-class Guitarist:
 
-class Bassist:
+class Guitarist(Musician):
+    def __str__(self):
+        return "My name is Joan Jett and I play guitar"
 
-class Drummer:
-    
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "guitar"
+
+    @staticmethod
+    def play_solo():
+        return "face melting guitar solo"
+
+
+class Bassist(Musician):
+    def __str__(self):
+        return "My name is Meshell Ndegeocello and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "bass"
+
+    @staticmethod
+    def play_solo():
+        return "bom bom buh bom"
+
+
+class Drummer(Musician):
+    def __str__(self):
+        return "My name is Sheila E. and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "drums"
+
+    @staticmethod
+    def play_solo():
+        return "rattle boom crash"
+
+
